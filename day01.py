9e4525c658e7151deee9ee30e8e165bc9caff8e2
Mark Stickling <mkstix6@gmail.com>
2021-12-04 16:14:20 +0000

    Answer for day01b

--- day01.py
+++ day01.py
@@ -12,4 +12,16 @@ def countDepthIncreases(data):
         current = x
     return count
 
-print (countDepthIncreases(data))
+def countDepthIncreasesInThrees(data):
+    count = 0
+    current = data[0] + data[1] + data[2] 
+    for i,x in enumerate(data):
+        if 0 <= i < len(data)-2:
+            windowsum = data[i] + data[i+1] + data[i+2] 
+            if windowsum > current:
+                count+=1
+            current = windowsum
+    return count
+
+print (f'Day01a: {countDepthIncreases(data)}')
+print (f'Day01b: {countDepthIncreasesInThrees(data)}')
--- test_day01.py
+++ test_day01.py
@@ -1,13 +1,17 @@
 import unittest
 
 from utils import importTXTFile
-from day01 import countDepthIncreases
+from day01 import countDepthIncreases, countDepthIncreasesInThrees
+
+exampledata = importTXTFile('day01-example.txt', int)
 
 class TestDay01(unittest.TestCase):
 
     def test_countDepthIncreases(self):
-        data = importTXTFile('day01-example.txt', int)
-        self.assertEqual(countDepthIncreases(data), 7)
+        self.assertEqual(countDepthIncreases(exampledata), 7)
+
+    def test_countDepthIncreasesInThrees(self):
+        self.assertEqual(countDepthIncreasesInThrees(exampledata), 5)
 
 if __name__ == '__main__':
     unittest.main()
