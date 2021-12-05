ea7014e6afee541109a76cd59c3ec2e573bfc7c4
Mark Stickling <mkstix6@gmail.com>
2021-12-05 16:13:37 +0000

    Answer for day03b

--- day03.py
+++ day03.py
@@ -4,15 +4,19 @@ with open("day03.txt", "r") as f:
     data = f.readlines()
 
 
-def powerConsumption(data):
-    return int(mostCommonBitNumber(data), 2) * int(leastCommonBitNumber(data), 2)
+def formatData(data):
+    return [[int(y) for y in word.strip()] for word in data]
+
+
+def transpose(matrix):
+    return list(zip(*matrix))
 
 
 def mostCommonBitNumber(data):
     # format data into lists of integers for later handling
-    intLists = [[int(y) for y in word.strip()] for word in data]
+    intLists = formatData(data)
     # transpose
-    charAtIndexCollections = list(zip(*intLists))
+    charAtIndexCollections = transpose(intLists)
     # count most common in each list
     mostCommons = [
         Counter(group).most_common(1)[0][0] for group in charAtIndexCollections
@@ -31,4 +35,45 @@ def leastCommonBitNumber(data):
     return binaryString
 
 
+def powerConsumption(data):
+    return int(mostCommonBitNumber(data), 2) * int(leastCommonBitNumber(data), 2)
+
+
+# To find oxygen generator rating, determine the most common value (0 or 1) in the current bit position, and keep only numbers with that bit in that position. If 0 and 1 are equally common, keep values with a 1 in the position being considered.
+def o2GeneratorRating(data) -> str:
+    index = 0
+    ifEqualCountsPerfer = 1
+    while len(data) > 1:
+        charIndexGroups = transpose(formatData(data))
+        mostCommon, countMostCommon = Counter(charIndexGroups[index]).most_common(1)[0]
+        # Case: check against equal count
+        if countMostCommon == len(charIndexGroups[index]) / 2:
+            mostCommon = ifEqualCountsPerfer
+        data = [x for x in data if int(x[index]) == mostCommon]
+        index += 1
+    return data[0].strip()
+
+
+# To find CO2 scrubber rating, determine the least common value (0 or 1) in the current bit position, and keep only numbers with that bit in that position. If 0 and 1 are equally common, keep values with a 0 in the position being considered.
+def co2ScrubberRating(data) -> str:
+    index = 0
+    ifEqualCountsPerfer = 0
+    while len(data) > 1:
+        charIndexGroups = transpose(formatData(data))
+        leastCommon, countLeastCommon = Counter(charIndexGroups[index]).most_common()[
+            -1
+        ]
+        # Case: check against equal count
+        if countLeastCommon == len(charIndexGroups[index]) / 2:
+            leastCommon = ifEqualCountsPerfer
+        data = [x for x in data if int(x[index]) == leastCommon]
+        index += 1
+    return data[0].strip()
+
+
+def lifeSupportRating(data) -> int:
+    return int(o2GeneratorRating(data), 2) * int(co2ScrubberRating(data), 2)
+
+
 print(f"Day03a: {powerConsumption(data)}")
+print(f"Day03b: {lifeSupportRating(data)}")
--- test_day03.py
+++ test_day03.py
@@ -1,6 +1,15 @@
 import unittest
 
-from day03 import powerConsumption, mostCommonBitNumber, leastCommonBitNumber
+from day03 import (
+    formatData,
+    transpose,
+    mostCommonBitNumber,
+    leastCommonBitNumber,
+    powerConsumption,
+    o2GeneratorRating,
+    co2ScrubberRating,
+    lifeSupportRating,
+)
 
 with open("day03-example.txt", "r") as f:
     exampledata = f.readlines()
@@ -11,8 +20,19 @@ with open("day03-example.txt", "r") as f:
 
 
 class TestDay03(unittest.TestCase):
-    def test_powerConsumption(self):
-        self.assertEqual(powerConsumption(exampledata), 198)
+    def test_formatdata(self):
+        # fmt:off
+        og1 = ['000','111','222']
+        og2 = ['000\n','111\n','222\n']
+        formatted = [[0,0,0],[1,1,1],[2,2,2]]
+        # fmt:on
+        self.assertEqual(formatData(og1), formatted)
+        self.assertEqual(formatData(og2), formatted)
+
+    def test_transpose(self):
+        og = [[0, 0, 0], [1, 1, 1], [2, 2, 2]]
+        transposed = [(0, 1, 2), (0, 1, 2), (0, 1, 2)]
+        self.assertEqual(transpose(og), transposed)
 
     def test_mostCommonBitNumber(self):
         self.assertEqual(mostCommonBitNumber(exampledata), "10110")
@@ -21,6 +41,20 @@ class TestDay03(unittest.TestCase):
     def test_leastCommonBitNumber(self):
         self.assertEqual(leastCommonBitNumber(exampledata), "01001")
         self.assertEqual(int(leastCommonBitNumber(exampledata), 2), 9)
+
+    def test_powerConsumption(self):
+        self.assertEqual(powerConsumption(exampledata), 198)
+
+    def test_o2GeneratorRating(self):
+        self.assertEqual(o2GeneratorRating(exampledata), "10111")
+        self.assertEqual(int(o2GeneratorRating(exampledata), 2), 23)
+
+    def test_co2ScrubberRating(self):
+        self.assertEqual(co2ScrubberRating(exampledata), "01010")
+        self.assertEqual(int(co2ScrubberRating(exampledata), 2), 10)
+
+    def test_lifeSupportRating(self):
+        self.assertEqual(lifeSupportRating(exampledata), 230)
 
 
 if __name__ == "__main__":
