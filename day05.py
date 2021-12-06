ceabc2bdddc2fa55c618f93bad12b36ed612ea7a
Mark Stickling <mkstix6@gmail.com>
2021-12-06 22:26:10 +0000

    Answer for 05a

---
+++ day05.py
@@ -0,0 +1,87 @@
+def ingestInput(data):
+    # In: '0,9 -> 5,9'
+    # Out: [[0,9,5,9]]
+    instructions = []
+    newline = []
+    for line in data:
+        points = line.split(" -> ")
+        for item in points:
+            pointpoint = item.split(",")
+            for p in pointpoint:
+                newline.append(int(p.strip()))
+        instructions.append(newline)
+        newline = []
+    return instructions
+
+
+def fieldSize(instructions):
+    xmin = 9999999
+    xmax = 0
+    ymin = 9999999
+    ymax = 0
+    for instruction in instructions:
+        x1, y1, x2, y2 = instruction
+        if min(x1, x2) < xmin:
+            xmin = min(x1, x2)
+        if min(y1, y2) < ymin:
+            ymin = min(y1, y2)
+        if max(x1, x2) > xmax:
+            xmax = max(x1, x2)
+        if max(y1, y2) > ymax:
+            ymax = max(y1, y2)
+    return (1 + xmax, 1 + ymax)
+
+
+def constructEmptyField(xsize, ysize):
+    emptyField = []
+    for y in range(0, ysize):
+        row = []
+        for x in range(0, xsize):
+            row.append(0)
+        emptyField.append(row)
+    return emptyField
+
+
+def fieldFromInstructions(instructions):
+    xrange, yrange = fieldSize(instructions)
+    field = constructEmptyField(xrange, yrange)
+    for instruction in instructions:
+        x1, y1, x2, y2 = instruction
+        if x1 == x2:
+            # sort y values ascending
+            if y1 > y2:
+                y1, y2 = y2, y1
+            # insert into field
+            for y in range(y1, y2 + 1):
+                field[y][x1] += 1
+        elif y1 == y2:
+            # sort x values ascending
+            if x1 > x2:
+                x1, x2 = x2, x1
+            # insert into field
+            for x in range(x1, x2 + 1):
+                field[y1][x] += 1
+    return field
+
+
+# At how many points do at least two lines overlap?
+def countDoublePlusPoints(field):
+    count = 0
+    for row in field:
+        for x in row:
+            if x > 1:
+                count += 1
+    return count
+
+
+def analyseInputForDoublePoints(data):
+    instructions = ingestInput(data)
+    fieldLayout = fieldFromInstructions(instructions)
+    return countDoublePlusPoints(fieldLayout)
+
+
+with open("day05-input.txt", "r") as f:
+    data = f.readlines()
+
+print(f"Day05a: {analyseInputForDoublePoints(data)}")
+# print(f"Day05b: {}")
---
+++ test_day05.py
@@ -0,0 +1,56 @@
+import unittest
+
+from day05 import (
+    ingestInput,
+    fieldSize,
+    constructEmptyField,
+    fieldFromInstructions,
+    countDoublePlusPoints,
+    analyseInputForDoublePoints,
+)
+
+with open("day05-example.txt", "r") as f:
+    exampledata = f.readlines()
+
+with open("day05-input.txt", "r") as f:
+    realdata = f.readlines()
+
+# fmt:off
+exampleInstructionList = [[0,9,5,9],[8,0,0,8],[9,4,3,4],[2,2,2,1],[7,0,7,4],[6,4,2,0],[0,9,2,9],[3,4,1,4],[0,0,8,8],[5,5,8,2]]
+exampleFieldLayout = [[0,0,0,0,0,0,0,1,0,0],[0,0,1,0,0,0,0,1,0,0],[0,0,1,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,0,0],[0,1,1,2,1,1,1,2,1,1],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[2,2,2,1,1,1,0,0,0,0]]
+# fmt:on
+
+
+class TestDay05(unittest.TestCase):
+    def test_ingestInput(self):
+        self.assertEqual(ingestInput(exampledata), exampleInstructionList)
+
+    def test_fieldSize(self):
+        self.assertEqual(fieldSize(exampleInstructionList), (10, 10))
+
+    def test_constructEmptyField(self):
+        self.assertEqual(constructEmptyField(1, 1), [[0]])
+        self.assertEqual(constructEmptyField(2, 2), [[0, 0], [0, 0]])
+        self.assertEqual(constructEmptyField(3, 3), [[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+        self.assertEqual(constructEmptyField(2, 3), [[0, 0], [0, 0], [0, 0]])
+        self.assertEqual(constructEmptyField(3, 2), [[0, 0, 0], [0, 0, 0]])
+
+    def test_fieldFromInstructions(self):
+        self.assertEqual(
+            fieldFromInstructions(exampleInstructionList), exampleFieldLayout
+        )
+
+    def test_countDoublePlusPoints(self):
+        self.assertEqual(countDoublePlusPoints(exampleFieldLayout), 5)
+
+    def test_analyseInputForDoublePoints(self):
+        self.assertEqual(analyseInputForDoublePoints(exampledata), 5)
+
+    def test_analyseInputForDoublePoints__RealAnswer(self):
+        self.assertEqual(
+            analyseInputForDoublePoints(realdata), 5306
+        )  # answer day05a confirmed
+
+
+if __name__ == "__main__":
+    unittest.main()
